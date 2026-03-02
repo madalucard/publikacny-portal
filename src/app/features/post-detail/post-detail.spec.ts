82a11ec9337f707daaf26ddc3469079b17e5340c
Martin Lapihuska <madalucard@gmail.com>
2026-03-02 19:54:30 +0100

    Post detail service test running

---
+++ src/app/features/post-detail/post-detail.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { PostsService } from '../../core/services/posts.service';
+
+describe('PostsService', () => {
+  let service: PostsService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PostsService, provideHttpClient(), provideHttpClientTesting()],
+    });
+    service = TestBed.inject(PostsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => httpMock.verify());
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
